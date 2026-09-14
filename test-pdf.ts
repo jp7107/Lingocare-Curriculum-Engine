79e59364ce3bcd3ac8ada32f591afca989713492
jp7107 <jpjangid710@gmail.com>
2026-09-14 10:42:24 +0530

    chore: remove test scripts that cause build failures

--- test-pdf.ts
+++
@@ -1,8 +0,0 @@
-import * as pdfjsLib from "pdfjs-dist/legacy/build/pdf.js";
-import { readFileSync } from "fs";
-
-async function run() {
-  const buffer = readFileSync("package.json"); // Just test if import works
-  console.log(typeof pdfjsLib.getDocument);
-}
-run();
